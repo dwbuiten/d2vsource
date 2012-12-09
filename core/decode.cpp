dwbuiten/d2vsource

commit 04033483c7677806cac16dcd7f545514f398bec0

Add support for multi-stream MPEG-TS files
d2vsource now respects the PID in the d2v file.

Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

core/decode.cpp
@@ -383,14 +383,21 @@ int decodeframe(int frame_num, d2vcontext *ctx, decodecontext *dctx, AVFrame *ou
         av_init_packet(&dctx->inpkt);
     }
 
-    /* Set our stream index if we need to. */
+    /*
+     * Set our stream index if we need to.
+     * Set it to the stream that matches our MPEG-TS PID if applicable.
+     */
     if (dctx->stream_index == -1) {
-        for(i = 0; i < dctx->fctx->nb_streams; i++) {
-            if (dctx->fctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
-                dctx->stream_index = (int) i;
-                break;
-            }
-        }
+        if (ctx->ts_pid)
+            for(i = 0; i < dctx->fctx->nb_streams; i++)
+                if (dctx->fctx->streams[i]->id == ctx->ts_pid)
+                    break;
+        else
+            for(i = 0; i < dctx->fctx->nb_streams; i++)
+                if (dctx->fctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
+                    break;
+
+        dctx->stream_index = (int) i;
     }
 
     /*

commit d7e1692557371239513027e9c242032c0c95c519

Fix mistakenly omitted braces
The inner if statment was being caught by the wrong else
statement.

Regression originally introduced in 04033483c7677806cac16dcd7f545514f398bec0.

Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

core/decode.cpp
@@ -388,14 +388,15 @@ int decodeframe(int frame_num, d2vcontext *ctx, decodecontext *dctx, AVFrame *ou
      * Set it to the stream that matches our MPEG-TS PID if applicable.
      */
     if (dctx->stream_index == -1) {
-        if (ctx->ts_pid)
+        if (ctx->ts_pid) {
             for(i = 0; i < dctx->fctx->nb_streams; i++)
                 if (dctx->fctx->streams[i]->id == ctx->ts_pid)
                     break;
-        else
+        } else {
             for(i = 0; i < dctx->fctx->nb_streams; i++)
                 if (dctx->fctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
                     break;
+        }
 
         dctx->stream_index = (int) i;
     }