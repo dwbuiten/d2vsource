dwbuiten/d2vsource

commit d1222be5983298e774b06614539657a34ef347ee

applyrff: Remove unnecessary variable

src/vs/applyrff.cpp
@@ -139,7 +139,6 @@ void VS_CC rffCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, 
     rffData *data;
     fieldFrame ff = { -1, -1 };
     string msg;
-    int total_fields;
     int i;
 
     /* Allocate our private data. */
@@ -166,7 +165,6 @@ void VS_CC rffCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, 
      * with which frames, and out total number of frames after
      * apply the RFF flags.
      */
-    total_fields = 0;
     data->frames.push_back(ff);
     for(i = 0; i < data->vi.numFrames; i++) {
         frame f = data->d2v->frames[i];
@@ -225,11 +223,9 @@ void VS_CC rffCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, 
             }
         }
         data->frames.push_back(ff);
-
-        total_fields += 2 + rff;
     }
 
-    data->vi.numFrames = total_fields / 2;
+    data->vi.numFrames = (int)data->frames.size();
 
     vsapi->createFilter(in, out, "applyrff", rffInit, rffGetFrame, rffFree, fmParallel, 0, data, core);
 }

commit 28033fa20f048a13f2e92bf9afeb561828cf6a2e

applyrff: Fix reported frame count
Broken in d1222be5983298e774b06614539657a34ef347ee.

src/vs/applyrff.cpp
@@ -250,7 +250,7 @@ void VS_CC rffCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, 
         }
     }
 
-    data->vi.numFrames = (int)data->frames.size();
+    data->vi.numFrames = (int)data->frames.size() - 1;
 
     vsapi->createFilter(in, out, "applyrff", rffInit, rffGetFrame, rffFree, fmParallel, 0, data, core);
 }